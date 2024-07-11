d6f0b01ed03c4735751a7eb99598d2ce8cc72cf0
Guadalupe Garcilazo <guadalupe.garcilazo@valtech.com>
2024-07-11 17:37:06 -0300

    Ejercicio de numeros perfectos, amigos y guay

--- src/algorithms.js
+++ src/algorithms.js
@@ -52,6 +52,42 @@ function verifyPrime(number) {
     }
     console.log(number, prime ? 'is prime' : 'is not prime');
 }
+// Verificar si un número es perfecto
+function verifyPerfect(number) {
+    let value = 0;
+    for (let i = 1; i < number; i++) {
+        if (number % i === 0)
+            value += i;
+    }
+    console.log(number, value === number ? 'is perfect' : 'is not perfect');
+}
+// Verificar si dos números son amigos
+function verifyFriends(a, b) {
+    let sumA = 0;
+    for (let i = 1; i <= a / 2; i++) {
+        if (a % i == 0)
+            sumA += i;
+    }
+    let sumB = 0;
+    for (let i = 1; i <= b / 2; i++) {
+        if (b % i == 0)
+            sumB += i;
+    }
+    console.log(a, b, (sumA === b && sumB === a) ? 'are friends' : 'are not friends');
+}
+// Verificar si un numero es guay
+function verifyGuay(number) {
+    let sum = 0;
+    let start = 1;
+    while (sum < number) {
+        sum += start;
+        if (sum === number) {
+            return true;
+        }
+        start++;
+    }
+    return false;
+}
 // Ejecucion de pruebas
 console.log('5 - 1 =', subAlg(5, 1));
 console.log('2 x 3 =', mulAlg(2, 3));
@@ -60,3 +96,9 @@ console.log('2^3 =', powAlg(2, 3));
 console.log('5! =', factAlg(5));
 verifyPrime(5);
 verifyPrime(4);
+verifyPerfect(6);
+verifyPerfect(29);
+verifyFriends(220, 284);
+verifyFriends(30, 42);
+console.log(verifyGuay(10));
+console.log(verifyGuay(7));
--- src/algorithms.ts
+++ src/algorithms.ts
@@ -56,6 +56,44 @@ function verifyPrime(number: number): void {
     console.log(number, prime ? 'is prime' : 'is not prime');
 }
 
+// Verificar si un número es perfecto
+function verifyPerfect(number: number): void {
+    let value = 0;
+    for (let i = 1; i < number; i++) {
+        if (number % i === 0) value += i;
+    }
+    console.log(number, value === number ? 'is perfect' : 'is not perfect');
+}
+
+// Verificar si dos números son amigos
+function verifyFriends(a: number, b: number): void {
+    let sumA = 0;
+    for (let i = 1; i <= a/2; i++) {
+        if (a % i == 0) sumA += i;
+    }
+    let sumB = 0;
+    for (let i = 1; i <= b/2; i++) {
+        if (b % i == 0) sumB += i;
+    }
+    console.log(a, b, (sumA === b && sumB === a) ? 'are friends' : 'are not friends');
+}
+
+// Verificar si un numero es guay
+function verifyGuay(number: number): boolean {
+    let sum = 0;
+    let start = 1;
+
+    while (sum < number) {
+        sum += start;
+        if (sum === number) {
+            return true;
+        }
+        start++;
+    }
+
+    return false;
+}
+
 // Ejecucion de pruebas
 console.log('5 - 1 =', subAlg(5, 1));
 console.log('2 x 3 =', mulAlg(2, 3));
@@ -64,3 +102,9 @@ console.log('2^3 =', powAlg(2, 3));
 console.log('5! =', factAlg(5));
 verifyPrime(5);
 verifyPrime(4);
+verifyPerfect(6)
+verifyPerfect(29)
+verifyFriends(220,284)
+verifyFriends(30, 42)
+console.log(verifyGuay(10))
+console.log(verifyGuay(7))
